298901e04e3a95927718cb47d0c1780d8734078d
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-28 16:55:05 +0100

    Create 01-streaming.py

---
+++ words-count/01-streaming.py
@@ -0,0 +1,38 @@
+class batchWC():
+    def __init__(self): #class constructor
+        self.base_data_dir = "/FileStore/data_spark_streaming_scholarnest"
+
+    def getRawData(self):
+        from pyspark.sql.functions import explode, split
+        lines = (spark.read #lines dataframe
+                    .format("text")
+                    .option("lineSep", ".")
+                    .load(f"{self.base_data_dir}/data/text")
+                )
+        return lines.select(explode(split(lines.value, " ")).alias("word")) #converting lines to words
+    
+    def getQualityData(self, rawDF):
+        from pyspark.sql.functions import trim, lower
+        return ( rawDF.select(lower(trim(rawDF.word)).alias("word"))
+                        .where("word is not null")
+                        .where("word rlike '[a-z]'") #word starts with 'a-z'
+                )
+        
+    def getWordCount(self, qualityDF):
+        return qualityDF.groupBy("word").count()
+    #creates aggregated result
+  
+    def overwriteWordCount(self, wordCountDF):
+        ( wordCountDF.write
+                    .format("delta")
+                    .mode("overwrite")
+                    .saveAsTable("word_count_table")
+        )
+    
+    def wordCount(self):
+        print(f"\tExecuting Word Count...", end='')
+        rawDF = self.getRawData()
+        qualityDF = self.getQualityData(rawDF)
+        resultDF = self.getWordCount(qualityDF)
+        self.overwriteWordCount(resultDF)
+        print("Done")
