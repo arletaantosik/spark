aa6868f35b150741c83ab0c22b94d585faa7dd1e
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-29 22:17:46 +0100

    Create 07-bronze.py

---
+++ medallion-approach/07-bronze.py
@@ -0,0 +1,36 @@
+class Bronze():
+    def __init__(self):
+        self.base_data_dir = "/FileStore/data_spark_streaming_scholarnest"
+
+    def getSchema(self):
+        return """InvoiceNumber string, CreatedTime bigint, StoreID string, PosID string, CashierID string,
+                CustomerType string, CustomerCardNo string, TotalAmount double, NumberOfItems bigint, 
+                PaymentMethod string, TaxableAmount double, CGST double, SGST double, CESS double, 
+                DeliveryType string,
+                DeliveryAddress struct<AddressLine string, City string, ContactNumber string, PinCode string, 
+                State string>,
+                InvoiceLineItems array<struct<ItemCode string, ItemDescription string, 
+                    ItemPrice double, ItemQty bigint, TotalValue double>>
+            """
+
+    def readInvoices(self):
+        return (spark.readStream
+                    .format("json")
+                    .schema(self.getSchema())
+                    #.option("cleanSource", "delete") # the old files that were processed by the previous micro-batch will be deleted, it's not working with all data sources (kafka, delta, jdbc,...)
+                    .option("cleanSource", "archive")
+                    .option("sourceArchiveDir", f"{self.base_data_dir}/data/invoices_archive") # folder where processed files are stored
+                    .load(f"{self.base_data_dir}/data/invoices") # landing zone
+                )  
+
+    def process(self):
+        print(f"\nStarting Bronze Stream...", end='')
+        invoicesDF = self.readInvoices()
+        sQuery =  ( invoicesDF.writeStream
+                            .queryName("bronze-ingestion")
+                            .option("checkpointLocation", f"{self.base_data_dir}/chekpoint/invoices_bz")
+                            .outputMode("append")
+                            .toTable("invoices_bz")           
+                    ) 
+        print("Done")
+        return sQuery   
