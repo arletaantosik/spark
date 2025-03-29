ed7da87af7d6fbfc3bafdf4ba3584b273a189cd9
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-29 21:57:02 +0100

    Create different-types.py

---
+++ streaming-sources-and-sinks/different-types.py
@@ -0,0 +1,12 @@
+# sources:
+  # file/directory
+  # delta table (databricks)
+  # kafka
+  # other connectors -> external connect
+
+# sinks:
+  # file/directory
+  # delta table
+  # kafka
+  # foreach - only append mode, MongoDb, snowflake, redis
+  # other connectors
