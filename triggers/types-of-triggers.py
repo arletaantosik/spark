3af2312ad7efb650de9f3c3be744d5ed3afa1cc2
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-29 20:11:58 +0100

    Update types-of-triggers.py

--- triggers/types-of-triggers.py
+++ triggers/types-of-triggers.py
@@ -21,6 +21,7 @@
 # 3. available-now
   # one-time micro-batch trigger to process all the available data and then stop on its own 
   # incremental batch - hours
+  # takes all data available in the landing zone, executes one micro batch, finishes and then streaming query automatically ends
   # if we want to run a trigger every one hour, we have to schedule an application to start every hour
   # more cost efficiency than the 2nd approach -> if we set that in 2nd approach, even the processing ends in 2 minutes, the resources are being used even if the batch is waiting for another 58 minutes
 
