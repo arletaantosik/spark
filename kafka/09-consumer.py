5e78f7e046455a148420840b2ac512fda7bb2660
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-03-30 17:26:14 +0200

    Create 09-consumer.py

---
+++ kafka/09-consumer.py
@@ -0,0 +1,20 @@
+# install library in databricks
+# library source: maven; coordinates: org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0 
+
+BOOTSTRAP_SERVER = "xyz:9092"
+JAAS_MODULE = "org.apache.kafka.common.security.plain.PlainLoginModule" #connecting from spark to kafka cluster, spark runs on JVM; security protocol, username, password handled by jaas_module in JVM
+CLUSTER_API_KEY = "xyz"
+CLUSTER_API_SECRET = "xyz"
+
+df = ( spark.read
+            .format("kafka")
+            .option("kafka.bootstrap.servers", BOOTSTRAP_SERVER)
+            .option("kafka.security.protocol", "SASL_SSL")
+            .option("kafka.sasl.mechanism", "PLAIN")
+            .option("kafka.sasl.jaas.config", f"{JAAS_MODULE} required username='{CLUSTER_API_KEY}' password='{CLUSTER_API_SECRET}';")
+            .option("subscribe", "invoices")
+            .load()
+        )
+
+
+display(df)
